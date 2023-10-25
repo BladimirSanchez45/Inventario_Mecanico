c24eb6e2077ffdca95a35155c0ea5f20fe85692e
alberto sanchez <al02963462@tecmilenio.mx>
2023-10-24 20:18:41 -0600

    Se agrega el proyecto

---
+++ php/conexion_be.php
@@ -0,0 +1,15 @@
+<?php
+
+
+    $conexion = mysqli_connect("localhost:3310","root","","login_register_db");
+
+    /*
+    if($conexion){
+        echo"Conectado exitosamente a la base de datos";
+    }else{
+        echo "No se ha podido conectar a la base de datos";
+    }
+    */
+
+
+?>
